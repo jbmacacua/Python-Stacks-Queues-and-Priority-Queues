1da85515cb9b1809d26463009615e661f6abe8c7
Jonas Brian R. Macacua <jbrmacacua03@gmail.com>
2022-12-14 18:26:17 +0800

    add the class IterableMixin

--- queue_7.py
+++ queue_7.py
@@ -2,6 +2,14 @@ from collections import deque
 from heapq import heappop, heappush
 from itertools import count
 
+class IterableMixin:
+    def __len__(self):
+        return len(self._elements)
+
+    def __iter__(self):
+        while len(self) > 0:
+            yield self.dequeue()
+
 class Queue:
     def __init__(self, *elements):
         self._elements = deque(elements)
