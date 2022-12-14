fa4e65f5bb12ed7b4f07eafec824bd6357713190
Jonas Brian R. Macacua <jbrmacacua03@gmail.com>
2022-12-14 18:19:24 +0800

    copy queue5

---
+++ queue_6.py
@@ -0,0 +1,34 @@
+from collections import deque
+from heapq import heappop, heappush
+
+class Queue:
+    def __init__(self, *elements):
+        self._elements = deque(elements)
+
+    def __len__(self):
+        return len(self._elements)
+
+    def __iter__(self):
+        while len(self) > 0:
+            yield self.dequeue()
+
+    def enqueue(self, element):
+        self._elements.append(element)
+
+    def dequeue(self):
+        return self._elements.popleft()
+
+class Stack(Queue):
+    def dequeue(self):
+        return self._elements.pop()
+
+class PriorityQueue:
+    def __init__(self):
+        self._elements = []
+
+    def enqueue_with_priority(self, priority, value):
+        heappush(self._elements, (-priority, value))
+
+    def dequeue(self):
+        return heappop(self._elements)[1]
+
