581ee098ea12b5cc1c4c1f5dcbbf2536b6a8ca0f
Jonas Brian R. Macacua <jbrmacacua03@gmail.com>
2022-12-17 13:59:58 +0800

    add the de sort_by and by_distance

---
+++ Graph7.py
@@ -0,0 +1,14 @@
+import networkx as nx
+from Graph3 import City, load_graph
+
+graph = nx.nx_agraph.read_dot("roadmap.dot")
+nodes, graph = load_graph("roadmap.dot", City.from_dict)
+
+def sort_by(neighbors, strategy):
+    return sorted(neighbors.items(), key=lambda item: strategy(item[1]))
+
+def by_distance(weights):
+    return float(weights["distance"])
+
+for neighbor, weights in sort_by(graph[nodes["london"]], by_distance):
+    print(f"{weights['distance']:>3} miles, {neighbor.name}")
