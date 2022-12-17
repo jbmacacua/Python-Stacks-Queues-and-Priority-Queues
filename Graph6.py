d09992f8bb418be23620958ec47ac0bcaf4338b2
Jonas Brian R. Macacua <jbrmacacua03@gmail.com>
2022-12-17 13:55:39 +0800

    print weights and neighbor

--- Graph6.py
+++ Graph6.py
@@ -2,3 +2,5 @@ from Graph3 import City, load_graph
 
 nodes, graph = load_graph("roadmap.dot", City.from_dict)
 
+for neighbor, weights in graph[nodes["london"]].items():
+    print(weights["distance"], neighbor.name)
